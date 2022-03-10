1cc24646ab6a6c2fa30ae01dc0b0df4fe014b3cb
Allison Yeh <63689595+allisonkyeh@users.noreply.github.com>
2022-03-10 09:41:01 -0500

    butterfly particles, audio, mats

---
+++ VRPalliative/Assets/ButterflyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ButterflyController : MonoBehaviour
+{
+    // [SerializeField] ParticleSystem bParticles;
+
+    // public SteamVR_Action_Boolean_Source grabPinch;
+
+    // private void Start {
+    //     SteamVR_Actions.default_GrabPinch.AddOnStateDownListener(TriggerPressed, SteamVR_Input_Sources.Any);
+
+    // }
+    // private void TriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource) {
+    //     bParticles.Play();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (grabPinch != null) {
+    //         bParticles.Play();
+    //     } else {
+    //         bParticles.Stop();
+    //     }
+    // }
+}
--- VRPalliative/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ VRPalliative/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Butterfly
+        {
+            get
+            {
+                return SteamVR_Actions.default_Butterfly;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
