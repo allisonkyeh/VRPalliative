a26ca92d1bd3b139e077edb8eba6de420722107b
Allison Yeh <63689595+allisonkyeh@users.noreply.github.com>
2022-03-24 00:27:27 -0400

    file org, butterfly trigger

---
+++ VRPalliative/Assets/_Butterfly/Scripts/ViveInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour
+{
+    public SteamVR_Action_Boolean GrabPinchAction;
+    private SteamVR_Input_Sources inputSource;
+    public ParticleSystem butterflies;
+
+    private void OnEnable() {
+        GrabPinchAction.AddOnStateDownListener(Press, inputSource);
+        GrabPinchAction.AddOnStateUpListener(PressUp, inputSource);
+    }
+
+    private void Press (SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log ("press!!");
+        butterflies.Play();
+    }
+    private void PressUp (SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log ("press UP!!");
+        butterflies.Stop();
+    }
+
+}
