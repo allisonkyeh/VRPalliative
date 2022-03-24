3d66fb4a94b629eb7d7894ecaad0cb7b5233babb
Allison Yeh <63689595+allisonkyeh@users.noreply.github.com>
2022-03-24 17:33:38 -0400

    pseudocode

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # VRPalliative
+Butterfly - 2019.4.31f1 URP
+Suncatcher - 
  
--- VRPalliative/Assets/_Suncatcher/Scripts/Worktable.cs
+++ VRPalliative/Assets/_Suncatcher/Scripts/Worktable.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Worktable : MonoBehaviour
 {
-    List<GameObject> toBreak;
-    
+    List<GameObject> piecesList;
+	// ????bool??? button
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void OnTriggerEnter () {
+        add to shardsList
+        // need some way to only add bottle pieces? or should whole bottles also be allowed
+        // then need to remove bottle if it’s broken, and add pieces when it breaks
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Assemble() {
+        // Scripting API: Transform.SetParent
+        // child.transform.SetParent(newParent);
+
+        // piece prefabs should be split before reaching this function
+
+        GameObject suncatcher;
+
+        foreach (GameObject piece in piecesList)
+        {
+            piece.transform.SetParent(suncatcher);
+            // add interactable component to suncatcher
+        }
     }
 }
