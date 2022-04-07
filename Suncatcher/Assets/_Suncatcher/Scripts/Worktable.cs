9e808fb255d946171e15d256f48a4a24b4920b9f
Allison Yeh <63689595+allisonkyeh@users.noreply.github.com>
2022-04-07 03:36:46 -0400

    project cleanup

---
+++ Suncatcher/Assets/_Suncatcher/Scripts/Worktable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worktable : MonoBehaviour
+{
+    List<GameObject> piecesList;
+	// ????bool??? button
+
+    void OnTriggerEnter () {
+        // add to shardsList
+        // need some way to only add bottle pieces? or should whole bottles also be allowed
+        // then need to remove bottle if it’s broken, and add pieces when it breaks
+
+    }
+
+    void Assemble() {
+        // Scripting API: Transform.SetParent
+        // child.transform.SetParent(newParent);
+
+        // piece prefabs should be split before reaching this function
+
+        GameObject suncatcher;
+
+        foreach (GameObject piece in piecesList)
+        {
+            // piece.transform.SetParent(suncatcher);
+            // add interactable component to suncatcher
+        }
+    }
+}
