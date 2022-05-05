57c03bc45c13250317b3f24c275b46fefac87ad1
Allison Yeh <63689595+allisonkyeh@users.noreply.github.com>
2022-05-05 12:48:52 -0400

    potpourri

--- Suncatcher/Assets/Scripts/Breaker.cs
+++ Suncatcher/Assets/Scripts/Breaker.cs
@@ -7,6 +7,7 @@ public class Breaker : MonoBehaviour
     private int breakLayer => LayerMask.NameToLayer("Breakable");
     private int handsLayer => LayerMask.NameToLayer("Hands");
     public Worktable table;
+    public ParticleSystem hitParticles;
 
     void Awake()
     {
@@ -16,7 +17,8 @@ public class Breaker : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         // Debug.Log("Hitting something on this layer: " + other.gameObject.layer);
-        
+        hitParticles.Emit(20);
+
         if (other.gameObject.layer == breakLayer){
             other.transform.root.gameObject.GetComponent<Bottle>().Break();
         }
